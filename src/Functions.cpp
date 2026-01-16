b507aa42a00baa3aa5b828d2ccd4702ea82fc0e1
“Ajr233” <werft206@gmail.com>
2026-01-16 17:43:14 -0500

    Worked on skills auto and slowed arm a bit

--- src/Functions.cpp
+++ src/Functions.cpp
@@ -224,14 +224,14 @@ void MatchLoading(){
 void ArmAction(){
   if(master.get_digital(DIGITAL_B) == true && IntakeLiftT == -1) {
     FrontIntake.move(127);  // Spin the intake motor when R1 is pressed
-    Arm.move_absolute(-570,190);  // Stop the intake motor when B is pressed
+    Arm.move_absolute(-570,150);  // Stop the intake motor when B is pressed
     KnownState=1;
     pros::delay(200);
     FrontIntake.move(0);  // Stop the intake motor when R2 is pressed
   }
   else if (master.get_digital(DIGITAL_L1) == true && (IntakeLiftT == -1 || IntakeLiftT == 0)){
     FrontIntake.move(-127);  // Spin the intake motor when R1 is pressed
-    Arm.move_absolute(-570,190);  // Stop the intake motor when B is released
+    Arm.move_absolute(-570,150);  // Stop the intake motor when B is released
     pros::delay(500);
     FrontIntake.move(0);  // Stop the intake motor when R2 is pressed
     Arm.move_absolute(5,200);  // Stop the intake motor when B is released
@@ -239,7 +239,7 @@ void ArmAction(){
   }
   else if(master.get_digital(DIGITAL_B) == true && IntakeLiftT == 1){
     FrontIntake.move(127);  // Spin the intake motor when R1 is pressed
-    Arm.move_absolute(-700,150);  // Stop the intake motor when B is pressed
+    Arm.move_absolute(-700,120);  // Stop the intake motor when B is pressed
     KnownState=1;
     pros::delay(200);
     FrontIntake.move(0);  // Stop the intake motor when R2 is pressed
